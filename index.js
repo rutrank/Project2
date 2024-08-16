990a4d1053a2738f7ac25412bd6c18cc9f3f8bf3
rutrank <137492467+rutrank@users.noreply.github.com>
2024-08-17 01:21:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("input")
+
+function reverseString(str) {
+
+  return str.split("").reverse().join("") //this is a new thing i have learnt which is using split function without anything will give each and every letter involved in the string
+
+}
+
+function check() {
+
+  const value = input.value
+  const reverse = reverseString(value)
+
+  if (value === reverse) {
+    alert("PALINDROME")
+  }
+  else {
+    alert("Nope")
+  }
+}
