4298e55151f355b46544049c49cb5361d2f3cfac
rutrank <137492467+rutrank@users.noreply.github.com>
2024-08-17 01:37:26 +0530

    update1

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const input = document.getElementById("input")
 
 function reverseString(str) {
 
-  return str.split("").reverse().join("") //this is a new thing i have learnt which is using split function without anything will give each and every letter involved in the string
+  return str.split("").reverse().join("").toLowerCase() //this is a new thing i have learnt which is using split function without anything will give each and every letter involved in the string
 
 }
 
 function check() {
 
-  const value = input.value
+  const value = input.value.toLowerCase()
   const reverse = reverseString(value)
 
   if (value === reverse) {
